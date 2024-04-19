eff4afecfa95f6fd390b078aa5cd52ac422e863b
TeBlanco <gio.cerda@duocuc.cl>
2024-04-19 15:46:18 +0000

    Radio 8

---
+++ R8.py
@@ -0,0 +1,13 @@
+import math
+
+def calcular_circ(radio):
+    pi = round(math.pi, 6)
+
+    circ = 2 * pi * radio
+    
+   return circ
+   radio  = 8
+
+  circ = calcular_circ(radio)
+
+print ("un radio de 8 cm es", radio, "cm" )
